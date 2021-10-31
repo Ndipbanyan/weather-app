7fe441e18016cc3fa3783264f688ce7f38b70cd7
Ndipbanyan <agnesjedidiah09@gmail.com>
2021-10-31 16:00:53 +0100

    update handler

--- src/_tests_/handlers.ts
+++ src/_tests_/handlers.ts
@@ -8,13 +8,76 @@ export const handlers = [
         ctx.status(200),
         ctx.json([
           {
-            id: 1635692400,
-            day: "Sun, October 31'",
-            Avg_temp: 301,
-            description: "light rain",
-            icon: "10d",
-            hourlytemp: [303.33, 301.8, 300.3],
-            hours: ["16:00 PM", "19:00 PM", "22:00 PM"],
+            dt: 1635692400,
+            main: {
+              temp: 288.95,
+              feels_like: 288.44,
+              temp_min: 286.35,
+              temp_max: 288.95,
+              pressure: 1006,
+              sea_level: 1006,
+              grnd_level: 946,
+              humidity: 71,
+              temp_kf: 2.6,
+            },
+            weather: [
+              {
+                id: 802,
+                main: "Clouds",
+                description: "scattered clouds",
+                icon: "03d",
+              },
+            ],
+            clouds: {
+              all: 34,
+            },
+            wind: {
+              speed: 2.12,
+              deg: 77,
+              gust: 3.09,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+              pod: "d",
+            },
+            dt_txt: "2021-10-31 15:00:00",
+          },
+          {
+            dt: 1635703200,
+            main: {
+              temp: 286.92,
+              feels_like: 286.31,
+              temp_min: 282.86,
+              temp_max: 286.92,
+              pressure: 1006,
+              sea_level: 1006,
+              grnd_level: 946,
+              humidity: 75,
+              temp_kf: 4.06,
+            },
+            weather: [
+              {
+                id: 802,
+                main: "Clouds",
+                description: "scattered clouds",
+                icon: "03n",
+              },
+            ],
+            clouds: {
+              all: 36,
+            },
+            wind: {
+              speed: 2.09,
+              deg: 169,
+              gust: 2.33,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+              pod: "n",
+            },
+            dt_txt: "2021-10-31 18:00:00",
           },
         ]),
         ctx.delay(150)
